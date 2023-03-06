668205a22d05b65b70f74cbc7c0d26eb34095773
Dima <11006851+DimaGalitskov@users.noreply.github.com>
2023-03-06 21:42:07 +0000

    Initial

---
+++ Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    CrawlerInput inputActions;
+    Rigidbody myRigidbody;
+    Vector3 moveDirection;
+    Quaternion lookRotation;
+    public float turnSpeed;
+    public float moveSpeed;
+
+    private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        inputActions = new CrawlerInput();
+        inputActions.Player.Move.started += MoveInput;
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Update()
+    {
+        myRigidbody.AddRelativeForce(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        Vector3 rbForward = myRigidbody.transform.forward;
+        Vector3 torque = Vector3.Cross(rbForward, moveDirection ) * turnSpeed * Time.deltaTime;
+        myRigidbody.AddTorque(torque);
+    }
+
+    void MoveInput(InputAction.CallbackContext context)
+    {
+        moveDirection = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
+        if (moveDirection != Vector3.zero)
+        {
+            lookRotation = Quaternion.LookRotation(moveDirection);
+        }
+    }
+}
---
+++ Trailer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trailer : MonoBehaviour
+{
+    GameObject player;
+    Vector3 moveDirection;
+    Quaternion lookRotation;
+    Rigidbody myRigidbody;
+    List<Vector3> positionHistory = new List<Vector3>();
+    public float turnSpeed;
+    public float moveSpeed;
+    public float delay;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        myRigidbody = GetComponent<Rigidbody>();
+        StartCoroutine(FollowTarget());
+
+
+    }
+
+    IEnumerator FollowTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+
+            Vector3 targetPosition = player.transform.position;
+            Vector3 currentPosition = transform.position;
+            Vector3 direction = (targetPosition - currentPosition).normalized;
+            Vector3 newPosition = currentPosition + direction * moveSpeed * Time.deltaTime;
+
+            transform.position = newPosition;
+        }
+    }
+}
