86fb49456ae2d651b0bc903888fcc7c95f1609fc
Dima <11006851+DimaGalitskov@users.noreply.github.com>
2023-03-17 15:06:37 +0000

    Throwing balls

---
+++ Launcher.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject ball;
+
+    private void Start()
+    {
+        StartCoroutine(ThrowBall());
+    }
+
+    IEnumerator ThrowBall()
+    {
+        while (true)
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
--- Pickup.cs
+++ Pickup.cs
@@ -6,11 +6,15 @@ public class Pickup : MonoBehaviour
 {
     GameObject player;
     GameObject gameController;
+    Rigidbody myRigidBody;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         gameController = GameObject.FindGameObjectWithTag("GameController");
+        myRigidBody = GetComponent<Rigidbody>();
+        Vector3 direction = (Vector3.forward + Vector3.up) * 10;
+        myRigidBody.AddRelativeForce(direction, ForceMode.Impulse);
     }
 
     private void OnTriggerEnter(Collider other)
